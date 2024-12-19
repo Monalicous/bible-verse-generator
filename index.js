580651ffca91f47f9063e62171a0ef545ae2a3ea
Monalicous <samukelisiwentusi@outlook.com>
2024-12-19 13:07:47 +0400

    changed some style and added some js layout

---
+++ index.js
@@ -0,0 +1,17 @@
+function pullVerse(event) {
+    event.preventDefault()
+
+}
+
+function showVerse(response) {
+    let apiKey =
+        let prompt =
+            let context =
+            let apiUrl =
+
+            axios.get(apiUrl).then(showVerse);
+
+}
+
+let submition = document.querySelector("#search-form");
+submition.addEventListener("submit", pullVerse);
