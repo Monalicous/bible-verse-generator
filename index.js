65ea405fb425eea7a59abb9152bfc4697f3e6592
Monalicous <samukelisiwentusi@outlook.com>
2024-12-20 18:56:27 +0400

    changed the context and the prompt for easier instructions

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function showVerse(response) {
 function pullVerse(event) {
     event.preventDefault();
 
-    let userInput = document.querySelector("#userInput");
+    let userInputElement = document.querySelector(".user-input");
 
     // AI api, context and prompt instructions
     let apiKey = "9a9b6ao3534d1b2afb7b49dddt30a6e7"
-    let prompt = "Your assignment is to generate a full verse from the bible, name the book, chapter number as well as the verse number in the title, write the information in HTML format and do not diplay the html tags, add the Verse AI signature at the end in a <strong> tag";
-    let context = `Generate bible verse from the book of ${userInput}`;
+    let prompt = "Your assignment is to generate a bible verses, name the book, chapter number as well as the verse number in the title, write the information in HTML format and do not diplay the html tags, add the Verse AI signature at the end in a <strong> tag";
+    let context = `Generate a verse from ${userInputElement}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     let mainElement = document.querySelector(".hidden");
     let verseElement = document.querySelector(".verse");
@@ -26,9 +26,7 @@ function pullVerse(event) {
     mainElement.classList.remove("hidden");
     verseElement.innerHTML = "Generating your verse...🙏🏽";
 
-
     axios.get(apiUrl).then(showVerse);
-
 }
 
 let submition = document.querySelector("#search-form");
