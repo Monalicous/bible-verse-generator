dd147388c670d2c19363f3f9d1f53fe592eabdc2
Monalicous <samukelisiwentusi@outlook.com>
2024-12-20 18:13:19 +0400

    Improved the user experience

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ function pullVerse(event) {
 
     // AI api, context and prompt instructions
     let apiKey = "9a9b6ao3534d1b2afb7b49dddt30a6e7"
-    let prompt = "Your assignment is to generate a full verse from the bible, name the book, chapter number as well as the verse number in the title, write the informatin in HTML format and do not diplay the html tags";
+    let prompt = "Your assignment is to generate a full verse from the bible, name the book, chapter number as well as the verse number in the title, write the information in HTML format and do not diplay the html tags, add the Verse AI signature at the end in a <strong> tag";
     let context = `Generate bible verse from the book of ${userInput}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let mainElement = document.querySelector(".hidden");
+    let verseElement = document.querySelector(".verse");
 
+    mainElement.classList.remove("hidden");
+    verseElement.innerHTML = "Generating your verse...🙏🏽";
     console.log("Generating your verse");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
+    console.log(`Prompt: ${prompt} `);
+    console.log(`Context: ${context} `);
 
     axios.get(apiUrl).then(showVerse);
 
